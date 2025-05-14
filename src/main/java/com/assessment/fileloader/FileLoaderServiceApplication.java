5de4d6181d2f4a57ac1ad251df3c13f143bc9ab1
Philip Narteh <Phinart98@gmail.com>
2025-05-14 21:49:22 +0000

    initial commit: starter project for file loader service

---
+++ src/main/java/com/assessment/fileloader/FileLoaderServiceApplication.java
@@ -0,0 +1,14 @@
+package com.assessment.fileloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileLoaderServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FileLoaderServiceApplication.class, args);
+	}
+
+}
