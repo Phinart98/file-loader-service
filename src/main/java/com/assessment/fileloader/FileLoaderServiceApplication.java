7dac9dd9b61496a4e3037f750fe89e173dfee4ea
Philip Narteh <Phinart98@gmail.com>
2025-05-15 14:18:57 +0000

    enabled scheduling for automatic file monitoring

--- src/main/java/com/assessment/fileloader/FileLoaderServiceApplication.java
+++ src/main/java/com/assessment/fileloader/FileLoaderServiceApplication.java
@@ -2,12 +2,13 @@ package com.assessment.fileloader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FileLoaderServiceApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(FileLoaderServiceApplication.class, args);
 	}
 
